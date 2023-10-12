971dd6494501a17827bee0534dc10fe2d70d8756
Emil Arktander <emar2202@student.miun.se>
2023-10-12 10:47:06 +0200

    Läggt till händelsehanterare för kontroll av inmatning. Färdigställt och testat att funktionen för  inmatningskontroll fungerar.

--- js/main.js
+++ js/main.js
@@ -6,18 +6,44 @@ Här lägger du din JavaScript-kod
 // Variabler
 let textEl = document.getElementById("newtodo");
 let newToDoBtnEl = document.getElementById("newtodobutton");
-let messageEl = document.getElementById("message");
+let errorMessageEl = document.getElementById("message");
 let toDoList = document.getElementById("todolist");
 let clearBtn = document.getElementById("clearbutton");
 
-// När sidan laddas in skriv ut lagrad data från web storage
-document.onload = printStorage();
+// När sidan laddas in initieras kod
+document.onload = pageLoad();
+
+// Händelshanterare
+
+// När en användare skriver in text i inmatningsfältet körs funktionen checkItemText()
+textEl.addEventListener("keyup", checkItemText);
+// När en användare klickar på lägg till-knappen körs funktionen addItem()
+newToDoBtnEl.addEventListener("click", addItem);
 
 // Funktioner
 
+// Kod som initieras när sidan laddas in
+function pageLoad() {
+  //Lägg till-knappen är oklickbar när sidan laddas in
+  newToDoBtnEl.disabled = true;
+}
+
 // Kontrollera inmatad text
 function checkItemText() {
-  console.log("Kontrollerar inmatad text...");
+  // Lagrar text från inmatningsfältet
+  let input = textEl.value;
+  // Kontrollerar om texten innehåller fler än fem tecken
+  if (input.length < 5) {
+    // Skriver ut ett felmeddelande ut på sidan
+    errorMessageEl.innerHTML = "Texten innehåller mindre än fem tecken!";
+    // Lägg till-knappen blir oklickbar
+    newToDoBtnEl.disabled = true;
+  } else {
+    // Rrensar eventuella felmeddelanden
+    errorMessageEl.innerText = "";
+    // Lägg till-knappen blir klickbar
+    newToDoBtnEl.disabled = false;
+  }
 }
 
 // Lägg till text i listan
